7ca5110274ae63b651d0af7adc7a563cca5caf5d
‘loitez’ <loitez@mail.ru>
2022-07-26 01:15:07 +0300

    solutions 3

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -86,13 +86,8 @@ function getArrayOfPositives(/* arr */) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(arr) {
-  for (let i = arr.length - 1; i <= 0; i -= 1) {
-    if (typeof arr[i] !== 'string') {
-      arr.slice(i);
-    }
-  }
-  return arr;
+function getArrayOfStrings(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 /**
